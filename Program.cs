978610dbd3216832bd2dc2f292a45cf74897de9f
Pro100Paladin <pro100paladin@icloud.com>
2023-08-25 18:36:08 +0300

    решение задачи 52

--- Program.cs
+++ Program.cs
@@ -1,39 +1,32 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, 
-// что такого элемента нет.
-
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// позиция i = 4, j = 2 -> такой позиции нет
-// позиция i = 1, j = 0 -> 5
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-Console.Write("Введите строку:");
-int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Введите столбец:");
-int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 5; // размер массива
-int m = 7; // размер массива
+
 Random random = new Random();
-int[,] arr = new int[n, m];
-Console.WriteLine("Исходный массив:");
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
 for (int i = 0; i < arr.GetLength(0); i++)
 {
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-        arr[i, j] = random.Next(10, 99);
-        Console.Write("{0} ", arr[i, j]);
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
     }
     Console.WriteLine();
 }
-if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+Console.WriteLine("-------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
 {
-    Console.WriteLine("Элемент не существует");
-}
-else
-{
-    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ Math.Round(sum / arr.GetLength(0),2)} ");
 }
 Console.WriteLine();
-
