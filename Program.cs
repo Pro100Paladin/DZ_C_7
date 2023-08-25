2591407a78da1392255f64c1d548229a9859f5ad
Pro100Paladin <pro100paladin@icloud.com>
2023-08-25 18:02:46 +0300

    решение задачи 50

--- Program.cs
+++ Program.cs
@@ -1,26 +1,39 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
 
-Console.WriteLine("Задайте количество строк двумерного массива:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте количество столбцов двумерного массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] twoDimArray = new double[m, n];
-Random rnd = new Random();
-void PrintArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { Console.Write($"{matr[i, j]} ");}
- Console.WriteLine();}}
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// позиция i = 4, j = 2 -> такой позиции нет
+// позиция i = 1, j = 0 -> 5
 
-void FillArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
-FillArray(twoDimArray);
+Console.Write("Введите строку:");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец:");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; // размер массива
+int m = 7; // размер массива
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(10, 99);
+        Console.Write("{0} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+    Console.WriteLine("Элемент не существует");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
 Console.WriteLine();
-PrintArray(twoDimArray);
 
